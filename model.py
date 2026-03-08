ef59f6d9138a22b00b8747327664732cb37523ff
kjm86 <kjm86@cam.ac.uk>
2026-03-08 20:23:18 +0000

    Implement calculation of negative sampling distribution and subsampling probabilities

--- model.py
+++ model.py
@@ -44,7 +44,52 @@ class Word2VecModel:
         :param seq: The sequence of string tokens
         :return: A matrix of size len(seq) by emb_size, containing token embeddings
         """
-
         one_hot: np.ndarray = self.one_hot_encode(seq)
         emb: np.ndarray = one_hot @ self.input_word_weights
         return emb
+
+    def _calculate_frequencies(self, one_hot_encodings: List[np.ndarray]) -> np.ndarray:
+        """
+        Calculates the frequencies of each token in the data.
+        :param one_hot_encodings: List of one-hot encodings of tokens in each sequence
+        :return: Frequencies for every token in the vocabulary
+        """
+        # count up how many times each token appeared in the data
+        counts_per_seq: np.ndarray = np.array([np.sum(one_hot, axis=0) for one_hot in one_hot_encodings])
+        total_counts: np.ndarray = np.sum(counts_per_seq, axis=0)
+
+        return total_counts / np.sum(total_counts)
+
+    def _calculate_neg_sampling_distr(self, freq: np.ndarray) -> np.ndarray:
+        """
+        Calculates the distribution used for negative sampling.
+        :param freq: Frequencies of words in the data
+        :return: Probability distribution for negative sampling
+        """
+        # calculate the distribution, specified in the paper as the unigram distribution raised to the 3/4 power
+        neg_sampling_distr: np.ndarray = np.pow(freq, 0.75)
+        neg_sampling_distr /= np.sum(neg_sampling_distr)
+
+        return neg_sampling_distr
+
+    def _calculate_discard_probabilities(self, freq: np.ndarray) -> np.ndarray:
+        """
+        Calculates the probabilities used for discarding more common tokens.
+        :param freq: Frequencies of words in the data
+        :return: Probability values for discarding tokens
+        """
+        # calculate the distribution, as specified in the paper
+        t = 0.005
+        discard_probabilities: np.ndarray = 1 - np.sqrt(t / np.maximum(freq, t))
+
+        return discard_probabilities
+
+    def train(self, train_data: List[List[str]], iter_num: int) -> None:
+        """
+        Main training loop. Performs the specified number of training iterations.
+        :param train_data: A list of sentences, divided into lists of string tokens
+        :param iter_num: The number of training iterations
+        """
+        one_hot_encodings = [self.one_hot_encode(seq) for seq in train_data]
+
+        freq = self._calculate_frequencies(one_hot_encodings)
